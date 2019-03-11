9863607002df35db2ba7d67d56e58401d0a1ab69
miratorimoonlight <kimmiratorimoonlight17@kit.edu.kh>
2019-03-11 23:03:44 +0700

    Updates

---
+++ precache-manifest.c39afd5cc2c60e226e36a0399e55e13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "618be408886a6d21caf8",
+    "url": "/react-todolist/static/js/runtime~main.618be408.js"
+  },
+  {
+    "revision": "ad5f07b0923e718bc121",
+    "url": "/react-todolist/static/js/main.ad5f07b0.chunk.js"
+  },
+  {
+    "revision": "e21667e23a659988597a",
+    "url": "/react-todolist/static/js/2.e21667e2.chunk.js"
+  },
+  {
+    "revision": "ad5f07b0923e718bc121",
+    "url": "/react-todolist/static/css/main.ad354f4f.chunk.css"
+  },
+  {
+    "revision": "e21667e23a659988597a",
+    "url": "/react-todolist/static/css/2.1c046910.chunk.css"
+  },
+  {
+    "revision": "b6bc6f4a5673897f94c0d9e1d3555026",
+    "url": "/react-todolist/index.html"
+  }
+];
