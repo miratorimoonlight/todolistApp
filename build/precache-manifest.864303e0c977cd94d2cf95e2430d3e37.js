8f607e60061595c428fa64b03869de3626be4ec0
miratorimoonlight <kimmiratorimoonlight17@kit.edu.kh>
2019-03-11 23:53:23 +0700

    change

--- build/precache-manifest.864303e0c977cd94d2cf95e2430d3e37.js
+++ build/precache-manifest.864303e0c977cd94d2cf95e2430d3e37.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/todolistApp/static/js/runtime~main.05fa5dd5.js"
   },
   {
-    "revision": "d415d981d2ab55721d78",
-    "url": "/todolistApp/static/js/main.d415d981.chunk.js"
+    "revision": "89787e10d8831e7ccc5b",
+    "url": "/todolistApp/static/js/main.89787e10.chunk.js"
   },
   {
     "revision": "e21667e23a659988597a",
     "url": "/todolistApp/static/js/2.e21667e2.chunk.js"
   },
   {
-    "revision": "d415d981d2ab55721d78",
+    "revision": "89787e10d8831e7ccc5b",
     "url": "/todolistApp/static/css/main.ad354f4f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/todolistApp/static/css/2.1c046910.chunk.css"
   },
   {
-    "revision": "4f57b63eec312dfc83c862f549786dcc",
+    "revision": "b4d67928f0893f22e95e341006acae3e",
     "url": "/todolistApp/index.html"
   }
 ];
